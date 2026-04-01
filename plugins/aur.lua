15ffbf7b8dccb8a0b72f396e47152db95fb4e6f1
NexushasTaken <nexushastaken@gmail.com>
2026-04-01 20:22:32 +0800

    refactor: refactor aur.lua plugin

--- plugins/aur.lua
+++ plugins/aur.lua
@@ -1,3 +1,37 @@
+local function dir_exist(dirpath)
+  local command = ("cd %s"):format(dirpath)
+  local success, _, _ = os.execute(command)
+  return success == true
+end
+
+local function is_git_repo(dirpath)
+  local command = ("cd %s && git rev-parse --is-inside-work-tree"):format(dirpath)
+  local success, _, _ = os.execute(command)
+  return success == true
+end
+
+local function get_command_output(command)
+  local handle, errmsg = io.popen(command)
+  if handle == nil then
+    print(("Error: %s"):format(errmsg))
+    return
+  end
+  local result = tostring(handle:read("*a"))
+  handle:close()
+  return result
+end
+
+local function string_split(inputstr, sep)
+  if sep == nil then
+    sep = "%s" -- Default to whitespace
+  end
+  local t = {}
+  for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
 ---@class Plugin
 return {
   name = "aur",
@@ -6,38 +40,57 @@ return {
     aur = {
       plan = function() end,
       exec = function(output)
-        local function dir_exist(dirpath)
-          local command = ("cd %s"):format(dirpath)
-          local success, _, _ = os.execute(command)
-          return success == true
-        end
-
-        local function is_git_repo(dirpath)
-          local command = ("cd %s && git rev-parse --is-inside-work-tree"):format(dirpath)
-          local success, _, _ = os.execute(command)
-          return success == true
-        end
-
-        local basename = output.repo:match("([^/]+)$")
-        local repodir = ("/var/tmp/%s"):format(basename)
-        os.execute("sudo pacman -S --needed base-devel git")
-
-        if dir_exist(repodir) then
-          if not is_git_repo(repodir) then
-            print(("Error: %s is not a valid git repository."):format(repodir))
+        local function get_package_list(repodir)
+          local get_pkg_files = ("cd %s && makepkg --packagelist"):format(repodir)
+          local raw_packages = get_command_output(get_pkg_files)
+          local packages_list = string_split(raw_packages, "\n")
+          for i, pkg in ipairs(packages_list) do
+            packages_list[i] = "./" .. string.sub(pkg, #repodir + 2)
           end
-        else
-          local clone = ("git clone %s %s"):format(output.repo, basename, repodir)
-          os.execute(clone)
+          return packages_list
         end
 
-        local build_command = ("cd %s && makepkg -s"):format(repodir)
-        local install_command = ("cd %s && makepkg -i --needed --noconfirm"):format(repodir)
-        local success, exitcode, code = os.execute(build_command)
-        -- code == 13 means "A package has already been built." - man makepkg #ERRORS
-        if (exitcode == "exit" and code == 13) or success then
-          os.execute(install_command)
+        local function setup_repodir()
+          local basename = output.repo:match("([^/]+)$")
+          local repodir = ("/var/tmp/%s"):format(basename)
+          os.execute("sudo pacman -S --needed base-devel git")
+
+          if dir_exist(repodir) then
+            if not is_git_repo(repodir) then
+              print(("Error: %s is not a valid git repository."):format(repodir))
+            end
+          else
+            local clone = ("git clone %s %s"):format(output.repo, basename, repodir)
+            os.execute(clone)
+          end
+
+          return repodir
         end
+
+        local function install(repodir)
+          local install_command = ("cd %s && makepkg -si --needed"):format(repodir)
+          local success, _, code = os.execute(install_command)
+          if success then
+            print("Command failed with exit code: " .. code)
+          else
+            print("Command exited with exit code: " .. code)
+          end
+        end
+
+        local function install2(repodir)
+          local build_command = ("cd %s && makepkg -s"):format(repodir)
+          local success, exitcode, code = os.execute(build_command)
+
+          -- code == 13 means "A package has already been built." - man makepkg #ERRORS
+          if (exitcode == "exit" and code == 13) or success then
+            local packages = table.concat(get_package_list(repodir), " ")
+            local install_command = ("cd %s && sudo pacman -U --needed %s"):format(repodir, packages)
+            os.execute(install_command)
+          end
+        end
+
+        local repodir = setup_repodir()
+        install(repodir)
       end,
     },
   },
